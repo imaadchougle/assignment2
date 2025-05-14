ce195b7d02f97f099a433ca99e6cf181d4f4c05b
imaadchougle <chougleimaad08@gmail.com>
2025-05-14 16:41:57 +0530

    Add files via upload

---
+++ as2.py
@@ -0,0 +1,13 @@
+
+a=int(input("enter a number:"))
+if a% 2==0:
+    print(f"{a}is even")
+else:
+    print(f"{a} is an odd")
+
+
+total_sum=0
+for number in range (1,51):
+    total_sum+=number
+    print(f"the sum of numbers from 1 to 50:,{total_sum}")
+
